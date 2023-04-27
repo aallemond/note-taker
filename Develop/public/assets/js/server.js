32f587569fdc743d0dd7b322a86be52e4b48f628
Andrew Allemond <118858494+aallemond@users.noreply.github.com>
2023-04-27 16:58:21 -0500

    finished pathing notes.html

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/notes', (req, res) => {
+  res.sendFile(__dirname + '/notes.html');
+});
+
 app.listen(3001, () => {
   console.log('Server listening on port 3001');
 });
--- Develop/routes/notes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-const uuid = require('../helpers/uuid');
-
-// GET Route for retrieving all the notes
-tips.get('/', (req, res) => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
